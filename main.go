0dac95131917eacd80ae518e155ab715635a5026
Otto Giron <otto.giron@xoom.com>
2017-04-25 12:37:18 -0600

    Modify main to instanciate hero controllers

--- main.go
+++ main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
-	"fmt"
+	"github.com/goadesign/goa"
+	"github.com/ottogiron/goa-heroes/app"
 )
 
 func main() {
-	fmt.Println("Hello world")
+	service := goa.New("heroes")
+
+	heroController := NewHeroController(service)
+
+	app.MountHeroController(service, heroController)
+
+	// Run service
+	if err := service.ListenAndServe(":8081"); err != nil {
+		service.LogError(err.Error())
+	}
 }
